b59bea60b2d1d3e5625396218a6528da17d6225c
Sam Dieck <samuel.dieck@gmail.com>
2016-04-21 14:14:52 -0700

    Still building more on Bloccit2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,62 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
-
   def show
     @post = Post.find(params[:id])
   end
 
   def new
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.new
   end
 
+  def create
+ # #9
+     @post = Post.new
+     @post.title = params[:post][:title]
+     @post.body = params[:post][:body]
+     @topic = Topic.find(params[:topic_id])
+
+     @post.topic = @topic
+
+ # #10
+     if @post.save
+ # #11
+       flash[:notice] = "Post was saved successfully."
+       redirect_to([@topic, @post])
+     else
+ # #12
+       flash.now[:alert] = "There was an error saving the post. Please try again."
+       render :new
+     end
+   end
+
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+     @post = Post.find(params[:id])
+     @post.title = params[:post][:title]
+     @post.body = params[:post][:body]
+
+     if @post.save
+       flash[:notice] = "Post was updated successfully."
+       redirect_to @post
+     else
+       flash.now[:alert] = "There was an error saving the post. Please try again."
+       render :edit
+     end
+   end
+
+   def destroy
+     @post = Post.find(params[:id])
+
+ # #8
+     if @post.destroy
+       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+       redirect_to([@topic, @post])
+     else
+       flash.now[:alert] = "There was an error deleting the post."
+       render :show
+     end
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :topics do
+     resources :posts, except: [:index]
+   end
+
   resources :posts
 
   get 'about' => 'welcome#about'

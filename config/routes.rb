c823202bf829c4e9e997672720519d5285a1773c
Sam Dieck <samuel.dieck@gmail.com>
2016-04-21 23:33:40 -0700

    Comments

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
-  
-  resources :posts
+
+  resources :posts, only: [] do
+     resources :comments, only: [:create, :destroy]
+   end
 
   get 'about' => 'welcome#about'
 

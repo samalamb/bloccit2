6e0ecf009def420df9cc4880058c492ef6450be6
Sam Dieck <samuel.dieck@gmail.com>
2016-04-23 08:28:32 -0700

    Assignment 45 completed, added more factories, made two new methods and, listed all favorited posts by user

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,4 +2,14 @@ module CommentsHelper
   def user_is_authorized_for_comment?(comment)
       current_user && (current_user == comment.user || current_user.admin?)
    end
+
+   def user_has_any_comments?(user)
+     if user.comments.any?
+       render(user.comments)
+     else
+       content_tag :p do
+         "#{user.name} doens't have any comments yet!"
+       end
+     end
+   end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,14 @@ module PostsHelper
   def user_is_authorized_for_post?(post)
         current_user && (current_user == post.user || current_user.admin?)
    end
+
+   def user_has_any_posts?(user)
+     if user.posts.any?
+       render(user.posts)
+     else
+       content_tag :p do
+         "#{user.name} doens't have any posts yet!"
+       end
+     end
+   end
 end
